953f95b9485d5b5efaa518b3269b2b1b2d8bc344
v_bilyanski <viktor.ch.bilyanski.2023@elsys-bg.org>
2026-05-31 22:32:43 +0300

    Update Account.h

--- Account.h
+++ Account.h
@@ -12,3 +12,18 @@ protected:
     string holderName;
     double balance;
     vector<Transaction> transactionHistory;
+public:
+    Account(string num, string name, double initialBalance);
+    virtual ~Account() = default;
+
+    virtual bool deposit(double amount);
+    virtual bool withdraw(double amount) = 0;
+    virtual void displayInfo() const;
+    virtual string getType() const = 0;
+
+    void displayHistory() const;
+    double getBalance() const;
+    string getAccountNumber() const;
+    string getHolderName() const;
+    void logExternalTransaction(string type, double amount);
+};
