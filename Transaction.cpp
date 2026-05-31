beeaf742b3d90fc2c31c8baf1752e6552ecb1249
v_bilyanski <viktor.ch.bilyanski.2023@elsys-bg.org>
2026-05-31 22:31:28 +0300

    Update Transaction.cpp

--- Transaction.cpp
+++ Transaction.cpp
@@ -11,3 +11,16 @@ static string getCurrentDate() {
     strftime(buf, sizeof(buf), "%d.%m.%Y", localtime(&now));
     return string(buf);
 }
+
+Transaction::Transaction(string t, double a, string d)
+    : type(t), amount(a), date(d.empty() ? getCurrentDate() : d) {}
+
+void Transaction::display() const {
+    cout << left << setw(22) << type
+         << " | " << setw(10) << date
+         << " | Suma: " << fixed << setprecision(2) << setw(10) << amount << " lv.\n";
+}
+
+string Transaction::getType() const { return type; }
+double Transaction::getAmount() const { return amount; }
+string Transaction::getDate() const { return date; }
