4fd0290c26133c776fe16439d94712a9b0f3da53
v_bilyanski <viktor.ch.bilyanski.2023@elsys-bg.org>
2026-05-31 22:28:43 +0300

    Create CheckingAccount.cpp

---
+++ CheckingAccount.cpp
@@ -0,0 +1,28 @@
+#include "CheckingAccount.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+CheckingAccount::CheckingAccount(string num, string name, double bal, double fee, double overdraft)
+    : Account(num, name, bal), transactionFee(fee), overdraftLimit(overdraft) {}
+
+bool CheckingAccount::withdraw(double amount) {
+    if (amount <= 0) {
+        cout << "  [GRESHKA] Sumata za teglene trqbva da e polozhitelna!\n";
+        return false;
+    }
+    double totalDeduction = amount + transactionFee;
+    // Allow overdraft up to the overdraftLimit
+    if ((balance + overdraftLimit) < totalDeduction) {
+        cout << "  [GRESHKA] Nedostatachno sredstva (vkl. taksa "
+             << fixed << setprecision(2) << transactionFee << " lv.)!\n";
+        return false;
+    }
+    balance -= totalDeduction;
+    transactionHistory.push_back(Transaction("Teglene (Razpl.)", amount));
+    if (transactionFee > 0) {
+        transactionHistory.push_back(Transaction("Taksa teglene", transactionFee));
+    }
+    return true;
+}
