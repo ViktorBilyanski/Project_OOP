8d131f67d8df622b672e8bc062f9d7f1dbde381d
v_bilyanski <viktor.ch.bilyanski.2023@elsys-bg.org>
2026-05-31 22:30:06 +0300

    Create SavingsAccount.cpp

---
+++ SavingsAccount.cpp
@@ -0,0 +1,32 @@
+#include "SavingsAccount.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+SavingsAccount::SavingsAccount(string num, string name, double bal, double rate, double minBal)
+    : Account(num, name, bal), interestRate(rate), minimumBalance(minBal) {}
+
+bool SavingsAccount::withdraw(double amount) {
+    if (amount <= 0) {
+        cout << "  [GRESHKA] Sumata za teglene trqbva da e polozhitelna!\n";
+        return false;
+    }
+    if ((balance - amount) < minimumBalance) {
+        cout << "  [GRESHKA] Tegleneto bi namalilo balansa pod minimuma ("
+             << fixed << setprecision(2) << minimumBalance << " lv.)!\n";
+        return false;
+    }
+    balance -= amount;
+    transactionHistory.push_back(Transaction("Teglene (Spest.)", amount));
+    return true;
+}
+
+void SavingsAccount::applyInterest() {
+    double interest = balance * interestRate;
+    balance += interest;
+    transactionHistory.push_back(Transaction("Lihva", interest));
+    cout << "  Nachislena e lihva (" << fixed << setprecision(1) << (interestRate * 100)
+         << "%): " << fixed << setprecision(2) << interest << " lv. -> Nov balans: "
+         << balance << " lv.\n";
+}
